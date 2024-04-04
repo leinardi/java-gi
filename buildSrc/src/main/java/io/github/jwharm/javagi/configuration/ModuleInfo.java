5123f43a8d5a200581bb2958e1c85fd8fbc62d7f
Jan-Willem Harmannij <jwharmannij@gmail.com>
2024-04-04 20:34:29 +0200

    Add spice-client-glib

--- buildSrc/src/main/java/io/github/jwharm/javagi/configuration/ModuleInfo.java
+++ buildSrc/src/main/java/io/github/jwharm/javagi/configuration/ModuleInfo.java
@@ -52,6 +52,7 @@ public final class ModuleInfo {
             entry("gsk",                       new Module("GSK", "org.gnome.gsk", "", "The rendering and scene graph API for GTK")),
             entry("gtk",                       new Module("GTK", "org.gnome.gtk", "https://docs.gtk.org/gtk4/", "GTK is a multi-platform toolkit for creating graphical user interfaces")),
             entry("gtksource",                 new Module("GtkSourceView", "org.gnome.gtksourceview", "https://gnome.pages.gitlab.gnome.org/gtksourceview/gtksourceview5/", "A text editor widget for code editing")),
+            entry("spiceclientglib",           new Module("SpiceClientGLib", "org.freedesktop.spice", "", "Java bindings for Spice-Client-GLib")),
             entry("adw",                       new Module("LibAdwaita", "org.gnome.adw", "https://gnome.pages.gitlab.gnome.org/libadwaita/doc/1-latest/", "Building blocks for modern GNOME applications")),
             entry("soup",                      new Module("LibSoup", "org.gnome.soup", "https://libsoup.org/libsoup-3.0/", "An HTTP client/server library for GNOME")),
             entry("javascriptcore",            new Module("JavaScriptCore", "org.gnome.webkit.jsc", "https://webkitgtk.org/reference/jsc-glib/stable/", "The JavaScript engine used in WebKit")),
